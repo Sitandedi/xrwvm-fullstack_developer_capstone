12caae3532f23ef87570e93695f4d87ba85a0981
Sitandedi <147408006+Sitandedi@users.noreply.github.com>
2025-10-25 00:10:50 -0400

    Uncomment timezone import and format year field

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,7 +2,7 @@
 
 
 from django.db import models
-#from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -34,7 +34,7 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -43,7 +43,8 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2026),
             MinValueValidator(2015)
